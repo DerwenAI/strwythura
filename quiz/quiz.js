d420d09967eef9f9f0373441b6ea7a3cd4ad8089
Paco Nathan <paco@derwen.ai>
2025-12-19 17:39:10 -0800

    prototype the formative assessment

---
+++ quiz/quiz.js
@@ -0,0 +1,78 @@
+const yay_emoji = [
+    "\u{1F601}",
+    "\u{1F44F}",
+];
+
+const sad_emoji = [
+    "\u{1F626}",
+    "\u{1F61F}",
+    "\u{1F62D}",
+];
+
+
+async function hash_digest (message) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message); 
+    const buffer = await crypto.subtle.digest("SHA-256", data); 
+    const explode = Array.from(new Uint8Array(buffer)); 
+    const hash = explode.map(byte => byte.toString(16).padStart(2, "0")).join(""); 
+
+    return hash;
+}
+
+
+async function score_quiz () {
+    const form = document.getElementById("multi_guess")
+    var score = 0;
+
+    const result = document.getElementById("result");
+    result.innerHTML = "";
+
+    Array.from(form.elements).forEach((input) => {
+        if (input.type == "radio") {
+	    const multi = document.getElementById(input.name);
+	    // clear any previous annotations
+	    input.labels[0].style.color = "black";
+	    input.labels[0].style.fontWeight = "normal";
+	    multi.innerHTML = "";
+
+	    if (input.checked) {
+    		const message = input.name.concat(input.id);
+
+		hash_digest(message).then(
+		    (hash) => {
+			var emoji = "";
+
+			if (answer_key.includes(hash)) {
+			    const idx = Math.floor(Math.random() * yay_emoji.length);
+			    emoji = yay_emoji[idx];
+			    input.labels[0].style.fontWeight = "bold";
+
+			    score += 1;
+			} else {
+			    const idx = Math.floor(Math.random() * sad_emoji.length);
+			    emoji = sad_emoji[idx];
+			    input.labels[0].style.color = "red";
+			};
+
+
+			const grade = [ Math.round(score / num_questions * 100.0).toString(), " / 100", ]
+			result.innerHTML = grade.join("");
+
+			// annotate the answer or decoy
+			const decoy = decoys.get(input.name);
+			var cite = "";
+
+			if (decoy[input.id].cite) {
+			    const link = [ "&nbsp;&nbsp;<a", ' target="_blank"', ' href="', decoy[input.id].cite, '">(ref)</a>', ];
+			    cite = link.join("");
+			};
+
+			const para = [ "<p><span style='font-size:2em;'>", emoji, "</span> <strong>", decoy[input.id].info, "</strong>", cite, "</p>", ];
+			multi.innerHTML = para.join("");
+		    }
+		);
+	    };
+        };
+    });
+}
---
+++ y.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""test rig"""
+
+import hashlib
+import json
+import pathlib
+import random
+
+from icecream import ic
+import jinja2
+
+
+if __name__ == "__main__":
+    # set up the Jinja2 environment and load the template
+    env: jinja2.Environment = jinja2.Environment(loader = jinja2.FileSystemLoader("quiz"))
+    template: jinja2.environment.Template = env.get_template("assess.html")
+
+    # render HTML using a JSON file
+    with open(pathlib.Path("quiz/ai_def.json"), "r", encoding = "utf-8") as fp:
+        dat: dict = json.load(fp)
+
+    # shuffle the order of questions and choices
+    for question in dat["questions"]:
+        random.shuffle(question["choices"])
+
+    random.shuffle(dat["questions"])
+
+    # generate SHA-256 for the answer key
+    dat["answer_key"] = []
+
+    for question in dat["questions"]:
+        for choice in question["choices"]:
+            if choice["correct"]:
+                message: str = question["name"] + choice["id"]
+                hash_digest: str = hashlib.sha256(message.encode()).hexdigest()
+                dat["answer_key"].append(hash_digest)
+
+    # render the template
+    html: str = template.render(
+        quiz = dat,
+    )
+
+    print(html)
