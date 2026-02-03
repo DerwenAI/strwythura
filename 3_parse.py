daebce6929f8f171cd96dee471a20a0ea1a2badf
Paco Nathan <paco@derwen.ai>
2026-02-03 21:19:51 +0000

    make pyInstrument optional, off by default to test GPU issues

--- 3_parse.py
+++ 3_parse.py
@@ -15,17 +15,16 @@ from strwythura import Profiler, Workflow
 
 
 if __name__ == "__main__":
-    # start the performance profiling -
-    profiling: bool = True # False
-
-    if profiling:
-        prof: Profiler = Profiler()
-
-
     # instantiate and configure our workflow manager
     work: Workflow = Workflow(
         config_path = pathlib.Path("config.toml"),
     )
+
+    # start the performance profiling
+    profiling: bool = work.config["prof"]["use_pyinst"]
+
+    if profiling:
+        prof: Profiler = Profiler()
 
     # de-serialize assets from previous steps
     work.thesaurus.load_source(
--- 6_vis.py
+++ 6_vis.py
@@ -15,18 +15,18 @@ from strwythura import Profiler, VisHTML, Workflow
 
 
 if __name__ == "__main__":
-    # start the performance profiling -
-    profiling: bool = True # False
-
-    if profiling:
-        prof: Profiler = Profiler()
-
-
     # instantiate and configure our workflow manager
     work: Workflow = Workflow(
         config_path = pathlib.Path("config.toml"),
     )
 
+    # start the performance profiling
+    profiling: bool = work.config["prof"]["use_pyinst"]
+
+    if profiling:
+        prof: Profiler = Profiler()
+
+    # finish provisioning
     work.ctx.erkg.load_graph(
         pathlib.Path(work.config["erkg"]["erkg_path"]),
     )
